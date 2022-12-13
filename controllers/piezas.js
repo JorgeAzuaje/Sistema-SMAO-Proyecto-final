80842060a6f916ad195999a6becae21d75130b6c
Jupagolab <114892574+Jupagolab@users.noreply.github.com>
2022-12-12 20:17:59 -0400

    Update piezas.js

--- controllers/piezas.js
+++ controllers/piezas.js
@@ -2,70 +2,105 @@ const {registroPiezas} = require('../models');
 const { db } = require('../models/nosql/proveedores');
 const mongoose = require('mongoose');
 
-/** 
+/ 
  * Obtener una vista de todas las piezas
  */
 
-const getPiezas = async (req, res) =>{
-    const user = req.user // TODO: NOs trae el usuario que hace un req.
-    const data = await registroPiezas.find({})
-    res.send({data, user})
-}
-
-/**
- * Registrar una piezas
- */
-const postPieza = async (req, res) =>{
-    const user = req.user
-    const _id = mongoose.Types.ObjectId();
-    const piezas = registroPiezas(req.body);
-    piezas
-    .save()
-    .then((data)=> res.send({data, user}))
-    .catch((error=> res.json({message: error})));
+const getPiezas =  async (req, res) => {
+    try {
+        const arrayPiezasDB = await registroPiezas.find()
+        res.render("piezas", {
+            arrayPiezas: arrayPiezasDB
+        })
+    } catch (error) {
+        console.log(Error)
+    }
 };
 
-/**
+/
+ * Registrar una piezas
+ */
+const postPiezas = (req, res) => {
+    res.render('crearPieza')
+}
+
+const postPieza = async (req, res ) => {
+    const body = req.body
+    try {
+        const piezaDB = new registroPiezas(body)
+        await piezaDB.save()
+        res.redirect('/api/piezas')
+    } catch (error) {
+        console.log('error', error)
+    }
+};
+
+/
  * Buscar pieza por ID
  */
 const getPieza = async (req, res) =>{
-    const user = req.user
-    const {_id} = req.params;
-    const data = await registroPiezas.findById(_id);
+    const id = req.params._id
+    try {
+        const piezasDB = await registroPiezas.findOne({ _id:id })
+        res.render('detallePieza', {
+            piezas: piezasDB,
+            error: false
+        })
+    } catch (error) {
+        res.render('detallePieza', {
+            mensaje:'No se ha encontrado el ID',
+            error: true
+        })
+    }
+};
 
-    res.send({data, user});
-}
-
-/**
+/
  * Modificar una pieza
  */
 const putPieza = async (req, res) => {
-    const user = req.user
-    const {_id} = req.params;
-    const {
-    marca,
-    tipo,
-    modelo,
-    descripcion,
-    estado
-    } = req.body;
-    const data = await registroPiezas.updateOne({_id: _id}, {$set: {
-        marca,
-        tipo,
-        modelo,
-        descripcion,
-        estado
-    }});
-    res.send({message:'La pieza ha sido actualizado exitosamente', user});
-}
+    const id = req.params._id
+    const body= req.body
+
+    try {
+        const piezaDB = await registroPiezas.findByIdAndUpdate(
+            id, body, { useFindAndModify: false }
+        )
+
+        res.json({
+            estado: true
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.json({
+            estado: false,
+            mensaje: 'Fallo al Editar'
+        })
+    }
+};
+
 
 /**
  * Eliminar una pieza
  */
-const deletePieza = async (req, res) =>{
-    const user = req.user
-    const {_id} = req.params;
-    const data = await registroPiezas.deleteOne({_id: _id});
-    res.send({message:'La pieza ha sido eliminado exitosamente', user});
+const deletePieza = async (req, res) => {
+    const id = req.params._id
+    try {
+        const piezasDB = await registroPiezas.findByIdAndDelete({ _id:id })
+        if(piezasDB) {
+            res.json({
+                estado: true,
+                mensaje: 'La pieza ha sido Eliminado'
+            })
+        } else {
+            res.json({
+                estado: false,
+                mensaje: 'La pieza no se ha podido Eliminar'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
 };
-module.exports = {getPiezas, postPieza, getPieza, putPieza, deletePieza};
+
+module.exports = {getPiezas, postPiezas, postPieza, getPieza, putPieza, deletePieza};
